bf28ac6dfe7aabe46e2167e4fca1ca7103a8c15f
Mário Santos <mariodev7@gmail.com>
2022-05-31 19:33:32 -0300

    create form sign in

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className={styles["login-page"]}>
+      <div className={styles["form-wrapper"]}>
+        <h1 className={styles.title}>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
