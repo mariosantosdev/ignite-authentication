fb132f90a674445aa8689c6b1e4e858d30567719
Mário Santos <mariodev7@gmail.com>
2022-06-01 09:56:15 -0300

    get user info in dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContexts";
+import { api } from "../services/api";
 
 export default function Dashboard() {
   const { user } = useAuth();
+
+  useEffect(() => {
+    api
+      .get("/me")
+      .then(({ data }) => console.log(data))
+      .catch(console.error);
+  });
 
   return <h1>Dashboard: {user?.email ?? ""}</h1>;
 }
