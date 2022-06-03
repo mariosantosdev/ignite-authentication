5956e45773fa6b770a7976eab520eddb0d98e3e8
Mário Santos <mariodev7@gmail.com>
2022-06-03 15:48:11 -0300

    signOut user when token is invalid

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
 
 import { signOut } from "../contexts/AuthContexts";
+import { AuthTokenError } from "../errors/AuthTokenError";
 
 let isRefreshing = false;
 let failedRequestedQueue = [];
@@ -79,6 +80,8 @@ export function setupAPIClient(ctx = undefined) {
         } else {
           if (process.browser) {
             signOut();
+          } else {
+            return Promise.reject(new AuthTokenError());
           }
         }
       }
--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -3,7 +3,8 @@ import {
   GetServerSidePropsContext,
   GetServerSidePropsResult,
 } from "next";
-import { parseCookies } from "nookies";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../errors/AuthTokenError";
 
 export function withSSRAuth<T>(fn: GetServerSideProps<T>): GetServerSideProps {
   return async (
@@ -20,6 +21,20 @@ export function withSSRAuth<T>(fn: GetServerSideProps<T>): GetServerSideProps {
       };
     }
 
-    return await fn(ctx);
+    try {
+      return await fn(ctx);
+    } catch (error) {
+      if (error instanceof AuthTokenError) {
+        destroyCookie(ctx, "auth.token");
+        destroyCookie(ctx, "auth.refreshToken");
+
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+    }
   };
 }
