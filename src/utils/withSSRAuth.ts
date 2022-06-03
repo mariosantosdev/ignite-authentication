1b42e6109fec78bd1dbec7d63beef06148601f02
Mário Santos <mariodev7@gmail.com>
2022-06-03 15:38:24 -0300

    create functional component to validate authenticate user

---
+++ src/utils/withSSRAuth.ts
@@ -0,0 +1,25 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export function withSSRAuth<T>(fn: GetServerSideProps<T>): GetServerSideProps {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<T>> => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies["auth.token"]) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
